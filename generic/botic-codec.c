1ebff9eda0822b5f7ce878c58af2b39eba5ee06b
Miroslav Rudisin <miero@seznam.cz>
2016-04-28 20:39:47 +0000

    add basic I2C control of ES9018 DAC

--- generic/botic-codec.c
+++ generic/botic-codec.c
@@ -36,6 +36,8 @@ struct botic_codec_data {
     struct i2c_client *i2c_client2;
     struct regmap *client1;
     struct regmap *client2;
+    int stream_muted;
+    int force_mute;
 };
 
 #define BOTIC_RATES (\
@@ -58,21 +60,17 @@ struct botic_codec_data {
 
 #define BOTIC_FORMATS (\
             SNDRV_PCM_FMTBIT_S16_LE | \
-            SNDRV_PCM_FMTBIT_S16_BE | \
             SNDRV_PCM_FMTBIT_S24_3LE | \
-            SNDRV_PCM_FMTBIT_S24_3BE | \
             SNDRV_PCM_FMTBIT_S24_LE | \
-            SNDRV_PCM_FMTBIT_S24_BE | \
             SNDRV_PCM_FMTBIT_S32_LE | \
-            SNDRV_PCM_FMTBIT_S32_BE | \
             SNDRV_PCM_FMTBIT_DSD_U8 | \
             SNDRV_PCM_FMTBIT_DSD_U16_LE | \
-            SNDRV_PCM_FMTBIT_DSD_U16_BE | \
             SNDRV_PCM_FMTBIT_DSD_U32_LE | \
-            SNDRV_PCM_FMTBIT_DSD_U32_BE | \
             0)
 
-static struct snd_soc_dai_driver botic_dac_dai = {
+static const struct snd_soc_dai_ops botic_codec_dai_ops;
+
+static struct snd_soc_dai_driver botic_codec_dai = {
     .name = BOTIC_CODEC_DAI_NAME,
     .playback = {
         .channels_min = 2,
@@ -90,6 +88,7 @@ static struct snd_soc_dai_driver botic_dac_dai = {
         .rates = BOTIC_RATES,
         .formats = BOTIC_FORMATS,
     },
+    .ops = &botic_codec_dai_ops,
 };
 
 static const struct snd_kcontrol_new botic_codec_controls[] = {
@@ -103,6 +102,7 @@ static int botic_codec_probe(struct snd_soc_codec *codec)
 {
     struct botic_codec_data *codec_data = snd_soc_codec_get_drvdata(codec);
     struct regmap_config config;
+    int ret;
 
     if (codec_data->i2c_adapter == NULL)
         return 0;
@@ -128,6 +128,25 @@ static int botic_codec_probe(struct snd_soc_codec *codec)
 
     codec_data->client2 =
         devm_regmap_init_i2c(codec_data->i2c_client2, &config);
+
+    /* Mute DAC */
+
+    ret = regmap_update_bits(codec_data->client1, 10, 0x01, 0x01);
+    if (ret != 0) {
+        dev_warn(codec->dev, "DAC#1 not found\n");
+        i2c_unregister_device(codec_data->i2c_client1);
+        codec_data->i2c_client1 = NULL;
+    }
+    ret = regmap_update_bits(codec_data->client2, 10, 0x01, 0x01);
+    if (ret != 0) {
+        dev_warn(codec->dev, "DAC#2 not found\n");
+        i2c_unregister_device(codec_data->i2c_client2);
+        codec_data->i2c_client2 = NULL;
+    }
+
+    /* Initialize codec params. */
+    codec_data->force_mute = 0;
+    codec_data->stream_muted = 1;
 
     if (0) {
         int i;
@@ -165,6 +184,9 @@ static unsigned int botic_codec_read(struct snd_soc_codec *codec,
     unsigned int t;
     int r = 0;
 
+    if (codec_data->client1 == NULL)
+        return 0;
+
     switch(reg) {
     case 0: /* Master Volume */
         r = regmap_read(codec_data->client1, 23, &t);
@@ -188,6 +210,9 @@ static unsigned int botic_codec_read(struct snd_soc_codec *codec,
         }
         v--;
         break;
+    case 1: /* Master Volume Mute */
+        v = codec_data->force_mute;
+        break;
     }
 
     if (!r)
@@ -201,22 +226,33 @@ static int botic_codec_write(struct snd_soc_codec *codec,
 {
     struct botic_codec_data *codec_data = snd_soc_codec_get_drvdata(codec);
     unsigned int t;
-    int r = 0;
+    int ret = 0;
+
+    if (codec_data->client1 == NULL)
+        return 0;
 
     switch(reg) {
     case 0: /* Master Volume */
         t = (1U << val) - 1;
-        r = regmap_write(codec_data->client1, 20, t & 0xff);
+        ret = regmap_write(codec_data->client1, 20, t & 0xff);
         t >>= 8;
-        r |= regmap_write(codec_data->client1, 21, t & 0xff);
+        ret |= regmap_write(codec_data->client1, 21, t & 0xff);
         t >>= 8;
-        r |= regmap_write(codec_data->client1, 22, t & 0xff);
+        ret |= regmap_write(codec_data->client1, 22, t & 0xff);
         t >>= 8;
-        r |= regmap_write(codec_data->client1, 23, t);
+        ret |= regmap_write(codec_data->client1, 23, t);
+        break;
+    case 1: /* Master Volume Mute */
+        codec_data->force_mute = val;
+        if (codec_data->force_mute) {
+            ret = regmap_update_bits(codec_data->client1, 10, 0x01, 0x01);
+        } else if (!codec_data->stream_muted) {
+            ret = regmap_update_bits(codec_data->client1, 10, 0x01, 0x00);
+        }
         break;
     }
 
-    if (!r)
+    if (!ret)
         return 0;
     else
         return -EIO;
@@ -229,6 +265,110 @@ static struct snd_soc_codec_driver botic_codec_socdrv = {
     .write = botic_codec_write,
     .controls = botic_codec_controls,
     .num_controls = ARRAY_SIZE(botic_codec_controls),
+};
+
+static int botic_codec_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
+{
+    struct snd_soc_codec *codec = dai->codec;
+    struct botic_codec_data *codec_data = snd_soc_codec_get_drvdata(codec);
+    int ret = 0;
+
+    if (codec_data->client1 == NULL)
+        return 0;
+
+    switch (fmt & SND_SOC_DAIFMT_FORMAT_MASK) {
+    case SND_SOC_DAIFMT_DIT:
+        ret = regmap_update_bits(codec_data->client1, 8, 0x80, 0x80);
+        break;
+    default:
+        ret = regmap_update_bits(codec_data->client1, 8, 0x80, 0x00);
+        break;
+    }
+
+    if (ret != 0)
+        return ret;
+
+    switch (fmt & SND_SOC_DAIFMT_FORMAT_MASK) {
+    case SND_SOC_DAIFMT_DIT:
+    case SND_SOC_DAIFMT_I2S:
+        ret = regmap_update_bits(codec_data->client1, 10, 0x30, 0x00);
+        break;
+    case SND_SOC_DAIFMT_LEFT_J:
+        ret = regmap_update_bits(codec_data->client1, 10, 0x30, 0x10);
+        break;
+    case SND_SOC_DAIFMT_RIGHT_J:
+        ret = regmap_update_bits(codec_data->client1, 10, 0x30, 0x20);
+        break;
+    default:
+        dev_warn(codec->dev, "unsupported DAI fmt %d", fmt);
+        ret = -EINVAL;
+        break;
+    }
+
+    return ret;
+}
+
+static int botic_codec_mute_stream(struct snd_soc_dai *dai, int mute, int stream)
+{
+    struct snd_soc_codec *codec = dai->codec;
+    struct botic_codec_data *codec_data = snd_soc_codec_get_drvdata(codec);
+    int ret = 0;
+
+    if (codec_data->client1 == NULL)
+        return 0;
+
+    if (stream != SNDRV_PCM_STREAM_PLAYBACK)
+        return ret;
+
+    codec_data->stream_muted = mute;
+    if (codec_data->stream_muted) {
+        ret = regmap_update_bits(codec_data->client1, 10, 0x01, 0x01);
+    } else if (!codec_data->force_mute) {
+        ret = regmap_update_bits(codec_data->client1, 10, 0x01, 0x00);
+    }
+
+    return ret;
+}
+
+static int botic_codec_hw_params(struct snd_pcm_substream *substream,
+        struct snd_pcm_hw_params *params, struct snd_soc_dai *dai)
+{
+    struct snd_soc_codec *codec = dai->codec;
+    struct botic_codec_data *codec_data = snd_soc_codec_get_drvdata(codec);
+    int ret;
+
+    if (codec_data->client1 == NULL)
+        return 0;
+
+    switch (params_format(params)) {
+    case SNDRV_PCM_FORMAT_S16_LE:
+        ret = regmap_update_bits(codec_data->client1, 10, 0xc0, 0x80);
+        break;
+
+    case SNDRV_PCM_FORMAT_S24_3LE:
+    case SNDRV_PCM_FORMAT_S24_LE:
+        ret = regmap_update_bits(codec_data->client1, 10, 0xc0, 0x00);
+        break;
+
+    case SNDRV_PCM_FORMAT_S32_LE:
+    case SNDRV_PCM_FORMAT_DSD_U8:
+    case SNDRV_PCM_FORMAT_DSD_U16_LE:
+    case SNDRV_PCM_FORMAT_DSD_U32_LE:
+        ret = regmap_update_bits(codec_data->client1, 10, 0xc0, 0xc0);
+        break;
+
+    default:
+        dev_warn(codec->dev, "unsupported PCM format %d", params_format(params));
+        ret = -EINVAL;
+    }
+
+    return ret;
+}
+
+static const struct snd_soc_dai_ops botic_codec_dai_ops = {
+    .set_fmt = botic_codec_set_fmt,
+    .mute_stream = botic_codec_mute_stream,
+    .hw_params = botic_codec_hw_params,
 };
 
 static int asoc_botic_codec_probe(struct platform_device *pdev)
@@ -258,7 +398,7 @@ static int asoc_botic_codec_probe(struct platform_device *pdev)
     dev_set_drvdata(&pdev->dev, codec_data);
 
     ret = snd_soc_register_codec(&pdev->dev,
-            &botic_codec_socdrv, &botic_dac_dai, 1);
+            &botic_codec_socdrv, &botic_codec_dai, 1);
 
     if (ret != 0) {
         i2c_put_adapter(codec_data->i2c_adapter);
